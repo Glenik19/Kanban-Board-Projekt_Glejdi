a51a3126c7a488c36b744fef4bfa351ff2226151
Glejdi <glenik19@htl-shkoder.com>
2025-10-07 10:46:57 +0200

    created component

---
+++ src/lib/components/Column.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { flip } from 'svelte/animate';
+
+  export let list = [];
+  export let startDrag;
+  export let onDrop;
+  export let onDragOver;
+</script>
+
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<section
+  class="bg-white h-[350px] w-[200px] space-y-2"
+  ondragover={onDragOver}
+  ondrop={onDrop}
+>
+  {#each list as item (item)}
+    <article
+      class="p-4 bg-blue-400"
+      draggable="true"
+      ondragstart={(e) => startDrag(item, e)}
+      animate:flip
+    >
+      {item}
+    </article>
+  {/each}
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Column from './components/Column.svelte';
+
+  let listA = [1,2,3];
+  let listB = [4];
+  let listC = [5,6,7];
+  let listD = [8,9];
+
+  function startDrag(item, event) {
+    event.dataTransfer.setData("text/plain", item);
+  }
+
+  function dragOver(event) {
+    event.preventDefault();
+  }
+
+  function moveItem(targetList) {
+    return (event) => {
+      let item = +event.dataTransfer.getData("text/plain");
+      listA = listA.filter(i => i !== item);
+      listB = listB.filter(i => i !== item);
+      listC = listC.filter(i => i !== item);
+      listD = listD.filter(i => i !== item);
+      targetList.push(item);
+    };
+  }
+</script>
+
+<h1 class="text-center">We learn now to drag and drop</h1>
+
+<main class="p-8 w-full bg-gray-500 h-[400px] flex justify-between items-center">
+  <Column list={listA} startDrag={startDrag} onDragOver={dragOver} onDrop={moveItem(listA)} />
+  <Column list={listB} startDrag={startDrag} onDragOver={dragOver} onDrop={moveItem(listB)} />
+  <Column list={listC} startDrag={startDrag} onDragOver={dragOver} onDrop={moveItem(listC)} />
+  <Column list={listD} startDrag={startDrag} onDragOver={dragOver} onDrop={moveItem(listD)} />
+</main>
